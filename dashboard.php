6ac9d9208c964e033a50d8f30a19cefc905f3eae
DiLLovsky <koperektbg@gmail.com>
2020-11-23 23:27:01 +0100

    Dashboard

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,11 @@ if (!isset($_SESSION['zalogowany'])) {
     exit();
 }
 
+$asd = $_SESSION['id_users_status'];
+if ($asd == '1') {
+    header('Location: adminpanel.php');
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,30 +20,88 @@ if (!isset($_SESSION['zalogowany'])) {
 <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE = edge, chrome-1" />
+    <meta name="viewport" content="width=device-width , initial-scale=1.0">
     <title>E-Gymnator</title>
+    <link rel="stylesheet" href="styleprofile.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+    <style>
+        body {
+            font-family: 'Poppins';
+        }
+    </style>
 </head>
 
 <body>
+    <input type="checkbox" id="check">
+    <header>
+        <label for="check">
+            <i class="fas fa-bars" id="sidebar_btn"></i>
+        </label>
+        <div class="left_area">
+            <h3>E-<span>Gymnator</span></h3>
+        </div>
+        <div class="right_area">
+            <a href="logout.php" class="logout_btn">Wyloguj się</a></div>
+    </header>
 
-    <?php
+    <div class="mobile_nav">
+        <div class="nav_bar">
+            <img src="halsey.jpg" class="mobile_profile_image" alt="">
+            <i class="fa fa-bars nav_btn"></i>
+        </div>
+        <div class="mobile_nav_items">
+            <a href="#"><i class="fas fa-home"></i><span>Dashboard</span></a>
+            <a href="#"><i class="fas fa-desktop"></i><span>xxx</span></a>
+            <a href="#"><i class="fas fa-home"></i><span>xxx</span></a>
+            <a href="#"><i class="fas fa-home"></i><span>xxx</span></a>
+            <a href="#"><i class="fas fa-weight"></i><span>Oblicz BMI</span></a>
+            <a href="profile.php"><i class="far fa-user"></i><span>Edytuj profil</span></a>
+        </div>
+    </div>
 
-    echo "<p>Witaj " . $_SESSION['login'] . '![<a href="logout.php">Wyloguj się!</a>]</p>';
+    <div class="sidebar">
+        <div class="profile_info">
+            <img src="halsey.jpg" class="profile_image" alt="">
+            <h4><?php echo $_SESSION['login'] ?></h4>
+        </div>
+        <a href="#"><i class="fas fa-home"></i><span>Dashboard</span></a>
+        <a href="#"><i class="fas fa-desktop"></i><span>xxx</span></a>
+        <a href="#"><i class="fas fa-home"></i><span>xxx</span></a>
+        <a href="#"><i class="fas fa-home"></i><span>xxx</span></a>
+        <a href="#"><i class="fas fa-weight"></i><span>Oblicz BMI</span></a>
+        <a href="profile.php"><i class="far fa-user"></i><span>Edytuj profil</span></a>
+    </div>
 
-    echo "<p>Trener " . $_SESSION['id_users'] . '![<a href="exercises.php">Wszystkie ćwiczenia</a>]</p>';
+    <div class="content">
+        <div class="card">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consectetur molestie dolor, sed elementum risus venenatis et. Praesent vitae orci sit amet erat sagittis suscipit ac nec turpis. Maecenas eu varius turpis. Pellentesque fermentum, lorem sit amet auctor dignissim, mauris velit venenatis neque, non vulputate sem lacus non diam. Curabitur eget scelerisque nulla. Donec augue libero, ullamcorper ac dictum sit amet, semper sodales quam. Mauris volutpat accumsan arcu ac porta. Sed lectus arcu, egestas sit amet fermentum sed, sollicitudin eu turpis. Vivamus nec efficitur ipsum, nec fermentum quam. Suspendisse nec volutpat tellus. Cras at consequat quam. Cras pretium eros ipsum, nec sagittis turpis gravida et.</p>
+        </div>
+        <div class="card">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consectetur molestie dolor, sed elementum risus venenatis et. Praesent vitae orci sit amet erat sagittis suscipit ac nec turpis. Maecenas eu varius turpis. Pellentesque fermentum, lorem sit amet auctor dignissim, mauris velit venenatis neque, non vulputate sem lacus non diam. Curabitur eget scelerisque nulla. Donec augue libero, ullamcorper ac dictum sit amet, semper sodales quam. Mauris volutpat accumsan arcu ac porta. Sed lectus arcu, egestas sit amet fermentum sed, sollicitudin eu turpis. Vivamus nec efficitur ipsum, nec fermentum quam. Suspendisse nec volutpat tellus. Cras at consequat quam. Cras pretium eros ipsum, nec sagittis turpis gravida et.</p>
+        </div>
+        <div class="card">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consectetur molestie dolor, sed elementum risus venenatis et. Praesent vitae orci sit amet erat sagittis suscipit ac nec turpis. Maecenas eu varius turpis. Pellentesque fermentum, lorem sit amet auctor dignissim, mauris velit venenatis neque, non vulputate sem lacus non diam. Curabitur eget scelerisque nulla. Donec augue libero, ullamcorper ac dictum sit amet, semper sodales quam. Mauris volutpat accumsan arcu ac porta. Sed lectus arcu, egestas sit amet fermentum sed, sollicitudin eu turpis. Vivamus nec efficitur ipsum, nec fermentum quam. Suspendisse nec volutpat tellus. Cras at consequat quam. Cras pretium eros ipsum, nec sagittis turpis gravida et.</p>
+        </div>
+        <div class="card">
+            <?php
+            echo "<p>Witaj " . $_SESSION['login'] . '![<a href="logout.php">Wyloguj się!</a>]</p>';
+            echo "<p>Ćwiczenia " . $_SESSION['id_users'] . '![<a href="exercises.php">Wszystkie ćwiczenia</a>]</p>';
+            echo '<p>Edytuj swój profil: [<a href="profile.php">Profil</a>]</p>';
+            $dataczas = new DateTime();
+            echo $dataczas->format('Y-m-d H:i:s');
+            ?>
+        </div>
+    </div>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('.nav_btn').click(function() {
+                $('.mobile_nav_items').toggleClass('active');
+            });
+        });
+    </script>
 
-    echo '<p>Edytuj swój profil: [<a href="profile.php">Profil</a>]</p>';
-
-    echo "<p>id_user_status: " . $_SESSION['id_users_status'] . "</p>";
-
-    echo '<p>Admin panel: [<a href="adminpanel.php">Profil</a>]</p>';
-
-    $dataczas = new DateTime();
-
-    echo $dataczas->format('Y-m-d H:i:s');
-
-
-
-    ?>
 
 </body>
 
